8c91c910e6ecaa084c3020c5e7b53f86c72130f8
agneevX <19761269+agneevX@users.noreply.github.com>
2021-06-18 16:20:01 +0530

    Fix lint

--- bash_scripts/spdtst.sh
+++ bash_scripts/spdtst.sh
@@ -3,19 +3,19 @@ set -e
 
 if hping3 1.0.0.1 -c 1 -S -p 80 &> /dev/null; then
   o=$(/usr/bin/speedtest -f json)
-  download_data_used="$(echo $o | jq '.download.bytes')"
-  upload_data_used="$(echo $o | jq '.upload.bytes')"
-  data_used=$((( $upload_data_used + $download_data_used ) / 1048786 ))
+  download_data_used="$(echo "$o" | jq '.download.bytes')"
+  upload_data_used="$(echo "$o" | jq '.upload.bytes')"
+  data_used=$((( upload_data_used + download_data_used ) / 1048786 ))
 
 #  download_time="$(echo $o | jq '.download.elapsed')"
 #  upload_time="$(echo $o | jq '.upload.elapsed')"
 #  total_test_time=$(( $download_time + $upload_time ))
 cat << EOF
 {
-  "download_speed": "$(echo $o | jq -r '.download.bandwidth')",
-  "upload_speed": "$(echo $o | jq '.upload.bandwidth')",
-  "Ping": "$(echo $o | jq -r '.ping.latency') ms",
-  "Server": "$(echo $o | jq '.server.name'|sed -e 's/^"//' -e 's/"$//')",
+  "download_speed": "$(echo "$o" | jq -r '.download.bandwidth')",
+  "upload_speed": "$(echo "$o" | jq '.upload.bandwidth')",
+  "Ping": "$(echo "$o" | jq -r '.ping.latency') ms",
+  "Server": "$(echo "$o" | jq '.server.name'|sed -e 's/^"//' -e 's/"$//')",
   "Data used": "$data_used MB"
 }
 EOF
