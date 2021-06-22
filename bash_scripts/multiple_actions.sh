c1bf6b2069aed9ee7d5fd5161a1df8a56b720608
agneevX <19761269+agneevX@users.noreply.github.com>
2021-06-22 20:48:47 +0530

    Use speedtest-tracker - properly name sensors

--- bash_scripts/multiple_actions.sh
+++ bash_scripts/multiple_actions.sh
@@ -1,16 +1,55 @@
 #!/bin/bash
+# shellcheck disable=SC2046,2005,2206
 
-if [[ "$1" == 'drive_state' ]]; then
-  mount_state=$(systemctl is-active mfs-drive.service)
-  drive_state=$(systemctl is-active rclone-drive.service)
-  crypt_state=$(systemctl is-active rclone-crypt.service)
+if [[ "$1" == "vnstat_daily_receive" ]]; then
+  ssh -i /config/scripts/falcon.key -o StrictHostKeyChecking=no agneev@127.0.0.1 vnstat -i eth0 --json d | jq '.interfaces[] | select(.name=="eth0")' | jq '.traffic.day[]' | grep 'rx' | tail -1 | echo $(tr -dc '0-9')
+fi
+
+if [[ "$1" == "vnstat_daily_sent" ]]; then
+  ssh -i /config/scripts/falcon.key -o StrictHostKeyChecking=no agneev@127.0.0.1 vnstat -i eth0 --json d | jq '.interfaces[] | select(.name=="eth0")' | jq '.traffic.day[]' | grep 'tx' | tail -1 | echo $(tr -dc '0-9')
+fi
+
+if [[ "$1" == *"speedtest"* ]]; then
+
+  o=$(curl -s "http://127.0.0.1:8700/api/speedtest/latest")
+
+  if [[ "$1" == *"download"* ]]; then
+    speed=$(echo "$o" | jq -r '.data.download')
+  else
+    speed=$(echo "$o" | jq -r '.data.upload')
+  fi
+
+cat << EOF
+{
+  "Ping": "$(echo "$o" | jq -r '.data.ping' | awk '{printf "%.0f\n", $1}') ms",
+  "speed": "$speed",
+  "Server": "$(echo "$o" | jq -r '.data.server_name')"
+}
+EOF
+
+fi
+
+if [[ "$1" == "drive_state" ]]; then
+
+o=$(ssh -i /config/scripts/falcon.key -o StrictHostKeyChecking=no agneev@127.0.0.1 "systemctl is-active mfs-drive.service && systemctl is-active rclone-drive.service && systemctl is-active rclone-drive.service")
+o=($o)
+
+  mount_state=${o[0]}
+  drive_state=${o[1]}
+  crypt_state=${o[2]}
+
   if [[ $mount_state == "active" ]]; then
     if [[ $drive_state == "active" ]]; then
       if [[ $crypt_state == "active" ]]; then
-        echo "all_mounted"
-      else echo "crypt_umount"; fi
+        echo "Online"
+      else
+        echo "Crypt down"
+      fi
     elif [[ $crypt_state == "active" ]]; then
-      echo "drive_umount"
-    else echo "all_umount"; fi
-  else echo "mfs_umount"; fi
+      echo "Drive down"
+    else
+      echo "Drive/Crypt down"
+    fi
+  else echo "Mount down"
+  fi
 fi
